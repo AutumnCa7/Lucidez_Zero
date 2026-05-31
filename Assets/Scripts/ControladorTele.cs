63d3fd110e0f1eaeda0a7656e7e9ba9217a50803
Niquito <nicolasalemanno@gmail.com>
2026-05-31 02:00:07 -0300

    JumpScare
    Se agrega un jumpscare en la tv1, en la habitación enfrente del primer respawn

--- Assets/Scripts/ControladorTele.cs
+++ Assets/Scripts/ControladorTele.cs
@@ -1,37 +1,59 @@
 using UnityEngine;
 using UnityEngine.Video;
+using System.Collections;
 
 public class ControladorTele : MonoBehaviour
 {
-    [Header("Componentes")]
+    [Header("Componentes de la Tele (Opcionales)")]
     public GameObject objetoPantalla;
     public VideoPlayer reproductorVideo;
+    public Light luzDeLaTele;
+
+    [Header("Configuración del Jumpscare")]
+    public GameObject imagenJumpscare;
+    public float dudaricionSusto = 1.5f;
 
     private bool estaEncendida = false;
 
     void Start()
     {
-        if (objetoPantalla != null)
-        {
-            objetoPantalla.SetActive(false);
-        }
+        if (objetoPantalla != null) objetoPantalla.SetActive(false);
+        if (luzDeLaTele != null) luzDeLaTele.enabled = false;
+        if (imagenJumpscare != null) imagenJumpscare.SetActive(false);
     }
 
     public void AlternarTelevisor()
     {
-        if (reproductorVideo == null || objetoPantalla == null) return;
-
         if (!estaEncendida)
         {
-            objetoPantalla.SetActive(true);
-            reproductorVideo.Play();
+            if (objetoPantalla != null) objetoPantalla.SetActive(true);
+            if (reproductorVideo != null) reproductorVideo.Play();
+            if (luzDeLaTele != null) luzDeLaTele.enabled = true;
+
+            if (imagenJumpscare != null)
+            {
+                StartCoroutine(RutinaJumpscare());
+            }
+
             estaEncendida = true;
         }
         else
         {
-            reproductorVideo.Stop();
-            objetoPantalla.SetActive(false);
+            if (reproductorVideo != null) reproductorVideo.Stop();
+            if (objetoPantalla != null) objetoPantalla.SetActive(false);
+            if (luzDeLaTele != null) luzDeLaTele.enabled = false;
+
             estaEncendida = false;
         }
+    }
+
+    private IEnumerator RutinaJumpscare()
+    {
+        imagenJumpscare.SetActive(true);
+        Debug.Log("¡BUUUH!");
+
+        yield return new WaitForSeconds(dudaricionSusto);
+
+        imagenJumpscare.SetActive(false);
     }
 }
